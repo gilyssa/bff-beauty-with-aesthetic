c50fc1918e8763d54fc2229d9d7132e9a62909e6
Lyssa <lyssa.lima@poli.digital>
2022-08-23 15:07:10 -0300

    feat: configurações de acesso ao banco

---
+++ src/database/config/config.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: __dirname+'/./../../../.env' });
+
+module.exports = {
+	development: {
+		username: process.env.DATABASE_USER,
+		password: process.env.PASSWORD,
+		database: process.env.DATABASE,
+		host: 'mysql04-farm36.kinghost.net',
+		dialect: 'mysql',
+		logging: false,
+		define: {
+			timestamps: true
+		},
+		timezone: '-03:00'
+	}
+}
