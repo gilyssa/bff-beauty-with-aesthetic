f4f49a2bd55da089fe1d4b1b44989df2dbcc60fb
Lyssa <lyssa.lima@poli.digital>
2022-08-31 17:09:49 -0300

    feat: adicionando rota de procedimento

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const handle404Error = require('./src/middlewares/handle404Error');
 const app = express();
 
 const usuarioRoute = require('./src/routes/usuario.route');
+const procedimentoRoute = require('./src/routes/procedimento.route');
+
 const handleError = require('./src/middlewares/handleError');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api/usuarios', usuarioRoute);
-
+app.use('/api/procedimento', procedimentoRoute);
 app.use(handle404Error);  
 app.use(handleError);
+
+
 app.listen(process.env.PORTA, () => { console.log('Api escutando na porta 3000!') })
 
 
