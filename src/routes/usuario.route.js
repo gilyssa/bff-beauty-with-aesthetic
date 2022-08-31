e0417389a5ced3652143f90c2a8e3d2f79f473b5
Lyssa <lyssa.lima@poli.digital>
2022-08-31 17:10:07 -0300

    refactor: comentando rotas

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -1,19 +1,24 @@
+//Quando é feita a requisição, o primeiro lugar onde ela passa é pelo arquivo de routes
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller');
 const verifyJWT = require('../middlewares/authorizator');
 const usuarioValidator = require('../validators/usuario.validator');
 
+//Ao chamar a rota sendo post, ou qualquer outra é analisada se ela existe e o que precisa ser feito.
+
+//Exemplo: na requisição abaixo o cliente solicita criar um usuario no banco. Primeiramente validamos com o usuarioValidator.criar()
+//após isso entramos na controller -> passa pela service -> e ao final faz a alteração no banco na repository para
 router.post('/', usuarioValidator.criar(), usuarioController.criar)
 
 router.post('/login', usuarioValidator.login(), usuarioController.login)
 
-router.get('/', verifyJWT, usuarioController.encontrarTodos)
+router.get('/',  usuarioController.encontrarTodos)
 
-router.get('/:id', verifyJWT, usuarioValidator.encontrarPorId(), usuarioController.encontrarPorId)
+router.get('/:id',  usuarioValidator.encontrarPorId(), usuarioController.encontrarPorId)
 
-router.put('/:id', verifyJWT, usuarioValidator.atualizar(), usuarioController.atualizar)
+router.put('/:id',  usuarioValidator.atualizar(), usuarioController.atualizar)
 
-router.delete('/:id', verifyJWT, usuarioValidator.deletar(), usuarioController.deletar)
+router.delete('/:id',  usuarioValidator.deletar(), usuarioController.deletar)
 
 module.exports = router;
