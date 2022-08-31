4a05354c4af14a1b59f0bf28530baa252c48ba53
Lyssa <lyssa.lima@poli.digital>
2022-08-31 17:10:25 -0300

    feat: adicionando mais validações de criar usuario

--- src/validators/usuario.validator.js
+++ src/validators/usuario.validator.js
@@ -4,8 +4,8 @@ const { validatorMessage } = require('../utils/errorMessage');
 const criar = function () {
 	return[
 		body('nome', validatorMessage('Nome')).exists().bail().isString(),
-		body('email', validatorMessage('Email')).exists().bail().isString(),
-		body('senha', validatorMessage('Senha')).exists().bail().isString(),
+		body('email', validatorMessage('Email')).exists().bail().isEmail(),
+		body('senha', validatorMessage('Senha')).exists().bail().isString().isLength({ min: 8}),
 	]
 }
 
