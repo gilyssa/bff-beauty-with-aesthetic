537f09c10c8504ef2777bcf7a9e4c73f7f403874
gilyssa <gilyssabrito@gmail.com>
2022-09-07 18:13:53 -0300

    feat: alteração heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,23 @@
 require('dotenv').config({ path: __dirname + '/./../../../.env' });
 
 module.exports = {
+  development: {
+    username: process.env.DATABASE_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: 'us-cdbr-east-06.cleardb.net',
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      timestamps: true,
+    },
+    timezone: '-03:00',
+  },
   production: {
     username: process.env.DATABASE_USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    host: 'mysql04-farm36.kinghost.net',
+    host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     logging: false,
     define: {
