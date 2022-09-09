e6cab362ee0f1e93501fcbc03062ee7e0f64f4f7
Lyssa <lyssa.lima@poli.digital>
2022-09-09 11:12:31 -0300

    make it better

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ app.use(cors(corsOptions));
 
 const usuarioRoute = require('./src/routes/usuario.route');
 const procedimentoRoute = require('./src/routes/procedimento.route');
-
+const indexRoute = require('./src/routes/index.route');
 const handleError = require('./src/middlewares/handleError');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/', indexRoute);
 app.use('/api/usuarios', usuarioRoute);
 app.use('/api/procedimentos', procedimentoRoute);
 app.use(handle404Error);
