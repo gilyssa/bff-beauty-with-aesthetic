b684fdf44dd1a93c20818e047973ea12d9ea9b4b
gilyssa <gilyssabrito@gmail.com>
2022-09-11 19:36:59 -0300

    adicionando decriptação

--- src/services/cliente.service.js
+++ src/services/cliente.service.js
@@ -4,8 +4,13 @@ require('dotenv').config();
 const { decrypt, encrypt } = require('../utils/encryptDecrypt');
 
 const criar = async function (cliente) {
+  cliente_CPF = cliente.cpf;
+  cliente_CPFCipher = encrypt(cliente_CPF);
+  cliente.cpf = JSON.stringify(cliente_CPFCipher);
+
   const existeCliente = await clienteRepository.encontrarUmPorWhere({
-    nome: cliente.nome,
+    cpf: cliente.cpf,
+    usuario_id: cliente.usuario_id,
   });
 
   if (existeCliente) {
@@ -23,10 +28,6 @@ const criar = async function (cliente) {
   clienteData_nascimento = cliente.data_nascimento;
   clienteData_nascimentoCipher = encrypt(clienteData_nascimento);
   cliente.data_nascimento = JSON.stringify(clienteData_nascimentoCipher);
-
-  cliente_CPF = cliente.cpf;
-  cliente_CPFCipher = encrypt(cliente_CPF);
-  cliente.cpf = JSON.stringify(cliente_CPFCipher);
 
   clienteTelefone = cliente.telefone;
   clienteTelefoneCipher = encrypt(clienteTelefone);
@@ -58,7 +59,45 @@ const atualizar = async function (cliente, id) {
 };
 
 const encontrarTodos = async function () {
-  const cliente = await clienteRepository.encontrarTodos();
+  let cliente = await clienteRepository.encontrarTodos();
+  var clientesTodos = cliente.map(function (cliente) {
+    clienteNome = JSON.parse(cliente.nome);
+    clienteNomeDecipher = decrypt(clienteNome);
+
+    clienteEmail = JSON.parse(cliente.email);
+    clienteEmailDecipher = decrypt(clienteEmail);
+
+    clienteData_nascimento = JSON.parse(cliente.data_nascimento);
+    clienteData_nascimentoDecipher = decrypt(clienteData_nascimento);
+
+    cliente_CPF = JSON.parse(cliente.cpf);
+    clienteCPFDecipher = decrypt(cliente_CPF);
+
+    clienteTelefone = JSON.parse(cliente.telefone);
+    clienteTelefoneDecipher = decrypt(clienteTelefone);
+
+    clienteTipo_sanguineo = JSON.parse(cliente.tipo_sanguineo);
+    clienteTipo_sanguineoDecipher = decrypt(clienteTipo_sanguineo);
+
+    clienteAlergias = JSON.parse(cliente.alergias);
+    clienteAlergiasDecipher = decrypt(clienteAlergias);
+
+    return {
+      id: cliente.id,
+      usuario_id: cliente.usuario_id,
+      createdAt: cliente.createdAt,
+      updatedAt: cliente.updatedAt,
+      nome: clienteNomeDecipher,
+      email: clienteEmailDecipher,
+      data_nascimento: clienteData_nascimentoDecipher,
+      cpf: clienteCPFDecipher,
+      telefone: clienteTelefoneDecipher,
+      tipo_sanguineo: clienteTipo_sanguineoDecipher,
+      alergias: clienteAlergiasDecipher,
+    };
+  });
+
+  cliente = clientesTodos;
   return cliente;
   //  clienteAlergiasDecipher = decrypt(JSON.parse(cliente.alergias));
   // cliente.alergias = clienteAlergiasDecipher;
@@ -70,6 +109,29 @@ const encontrarPorId = async function (id) {
   if (!cliente) {
     return createError(404, 'Cliente não encontrado');
   }
+  clienteNome = JSON.parse(cliente.nome);
+  clienteEmail = JSON.parse(cliente.email);
+  clienteData_nascimento = JSON.parse(cliente.data_nascimento);
+  cliente_CPF = JSON.parse(cliente.cpf);
+  clienteTelefone = JSON.parse(cliente.telefone);
+  clienteTipo_sanguineo = JSON.parse(cliente.tipo_sanguineo);
+  clienteAlergias = JSON.parse(cliente.alergias);
+
+  clienteNomeDecipher = decrypt(clienteNome);
+  clienteEmailDecipher = decrypt(clienteEmail);
+  clienteData_nascimentoDecipher = decrypt(clienteData_nascimento);
+  clienteCPFDecipher = decrypt(cliente_CPF);
+  clienteTelefoneDecipher = decrypt(clienteTelefone);
+  clienteTipo_sanguineoDecipher = decrypt(clienteTipo_sanguineo);
+  clienteAlergiasDecipher = decrypt(clienteAlergias);
+
+  cliente.nome = clienteNomeDecipher;
+  cliente.email = clienteEmailDecipher;
+  cliente.data_nascimento = clienteData_nascimentoDecipher;
+  cliente.cpf = clienteCPFDecipher;
+  cliente.telefone = clienteTelefoneDecipher;
+  cliente.tipo_sanguineo = clienteTipo_sanguineoDecipher;
+  cliente.alergias = clienteAlergiasDecipher;
 
   return cliente;
 };
--- src/utils/encryptDecrypt.js
+++ src/utils/encryptDecrypt.js
@@ -1,35 +1,45 @@
+// Node.js program to demonstrate the
+// crypto.createDecipheriv() method
+
 // Includes crypto module
 const crypto = require('crypto');
+
 // Difining algorithm
-const algorithm = process.env.AlGORITHM;
+const algorithm = 'aes-256-cbc';
+
 // Defining key
-const key = crypto.randomBytes(32);
+const key = '7wUuZ5hACUubh4BwvPW5oSadpa316qZT';
+
 // Defining iv
 const iv = crypto.randomBytes(16);
+
 // An encrypt function
-function encrypt(dados) {
+function encrypt(text) {
   // Creating Cipheriv with its parameter
   let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
-  // Updating dados
-  let encrypted = cipher.update(dados);
+
+  // Updating text
+  let encrypted = cipher.update(text);
+
   // Using concatenation
   encrypted = Buffer.concat([encrypted, cipher.final()]);
+
   // Returning iv and encrypted data
-  return {
-    iv: iv.toString(process.env.TYPE),
-    encryptedData: encrypted.toString(process.env.TYPE),
-  };
+  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
 
 // A decrypt function
-function decrypt(dados) {
-  let iv = Buffer.from(dados.iv, process.env.TYPE);
-  let encrypteddados = Buffer.from(dados.encryptedData, process.env.TYPE);
+function decrypt(text) {
+  let iv = Buffer.from(text.iv, 'hex');
+  let encryptedText = Buffer.from(text.encryptedData, 'hex');
+
   // Creating Decipher
   let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
-  // Updating encrypted dados
-  let decrypted = decipher.update(encrypteddados);
+
+  // Updating encrypted text
+  let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
+
   // returns data after decryption
   return decrypted.toString();
 }
